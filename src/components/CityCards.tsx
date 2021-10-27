ba5141d2c13d68c3c5bdd7fc86ca0a69e584c753
Octavian Regatun <octavian.regatun@outlook.com>
2021-10-27 03:21:18 +0300

    fix: location history search bug

--- src/components/CityCards.tsx
+++ src/components/CityCards.tsx
@@ -3,10 +3,9 @@ import CityCard from "./CityCard";
 import styles from "@/styles/CityCards.module.css";
 import { useHistoryLocationStore } from "@/lib/store";
 
+let counter = 0;
+
 export default function CityCards() {
-  useEffect(() => {
-    console.log("update");
-  });
   const historyLocations = useHistoryLocationStore(
     state => state.historyLocations,
     (prevState, newState) => prevState.queue !== newState.queue
@@ -14,7 +13,7 @@ export default function CityCards() {
 
   const renderHistoryLocations = historyLocations.queue.map(historyLocation => (
     <CityCard
-      key={historyLocation.id}
+      key={`${historyLocation.id}-${counter++}`}
       photoSrc="https://i.pinimg.com/originals/0a/ee/68/0aee68e5e233cad0c948e3789d08f085.jpg"
       title={historyLocation.title}
     />
